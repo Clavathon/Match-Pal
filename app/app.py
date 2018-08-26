0bbad35016228c2486330174c1940791b018772c
Rob Wong <robwong@Mukuls-MacBook-Pro-2.local>
2018-08-25 20:30:16 -0700

    need to fix connection to heroku psql db. Need to also add procfile to start gunicorn

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ import datetime
 
 from flask import Flask, request, jsonify, make_response, redirect, url_for, render_template, Blueprint
 from json import loads
+
+from flask_sqlalchemy import SQLAlchemy
 import logging
 
 
@@ -17,6 +19,27 @@ TEMPLATE_FOLDER = os.path.join(APP_DIR, 'react_folder/build/')
 app = Flask(__name__, static_folder="../react_folder/build/static",
             template_folder='../react_folder/build/')
 
+'''
+DO
+NOT
+LEAVE
+IN
+PRODUCTION
+'''
+
+
+host = os.environ['host']
+db = os.environ['db']
+user = os.environ['user']
+port = os.environ['port']
+pwd = os.environ['pwd']
+
+query = 'postgresql://{user}:{pwd}@{host}:{port}/{db}'.format(
+    user=user, pwd=pwd, host=host, port=port, db=db)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = query
+
+odb = SQLAlchemy(app)
 
 # app.secret_key = os.environ['APP_SECRET_KEY']
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
@@ -26,6 +49,19 @@ app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
 # app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 
 app.config['JWT_SECRET_KEY'] = 'SUPER-HOT'
+
+
+class User(odb.Model):
+    id = odb.Column(odb.Integer, primary_key=True)
+    name = odb.Column(odb.String(80))
+    email = odb.Column(odb.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
 @app.route('/', methods=['GET'])
