5b5c5cf9648a501d80110c021ddad3c2feeeaece
Rob Wong <robwong@Mukuls-MacBook-Pro-2.local>
2018-08-25 17:29:18 -0700

    Am able to serve react app from front end" "

--- app/app.py
+++ app/app.py
@@ -6,7 +6,17 @@ from json import loads
 import logging
 
 
-app = Flask(__name__)
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+print(APP_DIR, " current dir\n\n")
+STATIC_FOLDER = os.path.join(APP_DIR, 'react_folder/build/static')
+print(STATIC_FOLDER)
+TEMPLATE_FOLDER = os.path.join(APP_DIR, 'react_folder/build/')
+
+
+# app = Flask(__name__, static_folder=STATIC_FOLDER, template_folder=TEMPLATE_FOLDER)
+app = Flask(__name__, static_folder="../react_folder/build/static",
+            template_folder='../react_folder/build/')
+
 
 # app.secret_key = os.environ['APP_SECRET_KEY']
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
@@ -18,9 +28,13 @@ app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
 app.config['JWT_SECRET_KEY'] = 'SUPER-HOT'
 
 
+@app.route('/', methods=['GET'])
+def home_page():
+    return render_template('index.html')
+
+
 @app.route('/login', methods=['GET'])
 def login_page():
-
     return "it's working"
 
 
