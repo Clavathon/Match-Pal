af128e1547c9e93f91d97d66339ff9675c05a919
Kenny Le <kennyle37@gmail.com>
2018-08-25 17:35:35 -0700

    small test from rob side, venv testing

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 const Login = () => (
   <div className='login-form'>
@@ -32,7 +37,7 @@ const Login = () => (
           </Segment>
         </Form>
         <Message>
-          Don't Have an Account? <a href='#'>Register</a>
+          Don't Have an Account? <Link to='/register'>Register</Link>
         </Message>
       </Grid.Column>
     </Grid>
@@ -40,3 +45,19 @@ const Login = () => (
 )
 
 export default Login;
+
+{/* <Router>
+<div>
+  <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/topics">Topics</Link></li>
+  </ul>
+
+  <hr/>
+
+  <Route exact path="/" component={Home}/>
+  <Route path="/about" component={About}/>
+  <Route path="/topics" component={Topics}/>
+</div>
+</Router> */}
